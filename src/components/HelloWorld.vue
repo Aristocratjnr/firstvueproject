f6c7129116b0eeb6720ed36bfd273afc9468dbbd
Obuobi Ayim David <102563270+Aristocratjnr@users.noreply.github.com>
2024-05-14 15:03:22 +0000

    refactor: Update HelloWorld.vue to display project status

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,17 +14,10 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Status:
+      <code>My First Vue project</code> 
     </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
 </template>
 
 <style scoped>
